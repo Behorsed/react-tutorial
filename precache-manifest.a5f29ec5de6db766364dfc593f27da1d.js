127a753c982d0cda6116b57ea35947636579b1ac
Behorsed <paulina.e.kowalik@wp.pl>
2019-06-26 23:13:34 +0200

    Updates

---
+++ precache-manifest.a5f29ec5de6db766364dfc593f27da1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b20349c61eb548c0cd256e72fae58f",
+    "url": "/Behorsed/react-tutorial/index.html"
+  },
+  {
+    "revision": "2c2b67eacb0bc8e36d72",
+    "url": "/Behorsed/react-tutorial/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/Behorsed/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "2c2b67eacb0bc8e36d72",
+    "url": "/Behorsed/react-tutorial/static/js/main.ae9b336a.chunk.js"
+  },
+  {
+    "revision": "f70f88acd815d51bbe5c",
+    "url": "/Behorsed/react-tutorial/static/js/runtime~main.1b93c2f5.js"
+  }
+]);
